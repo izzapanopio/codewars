12ec6c1ba65c5cdd093110962136fd9c3030701d
izzapanopio <37319263+izzapanopio@users.noreply.github.com>
2018-03-14 10:13:28 +0800

    Create duplicate_encoder.php

---
+++ duplicate_encoder.php
@@ -0,0 +1,39 @@
+<?php
+/*
+The goal of this exercise is to convert a string to a new string where each character in the new string is '(' if that character appears only once in the original string, or ')' if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+Examples:
+
+"din" => "((("
+
+"recede" => "()()()"
+
+"Success" => ")())())"
+
+"(( @" => "))(("
+
+
+Notes:
+
+There is a flaw in the JS version, that may occur in the random tests. Do not hesitate to do several attempts before modifying your code if you fail there. 
+
+Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is actually the expected result, not the input! (these languages are locked so that's not possible to correct it).
+*/
+
+
+function duplicate_encode($word){
+	// ...
+  $word = str_split(strtolower($word));
+  $counter = array_count_values($word);
+ 
+  $decodedString = "";
+  foreach($word as $key => $value){
+   
+    $char_count = $counter[$value];
+    
+    $decodedString .= ($char_count == 1) ? "(" : ")";
+  }
+  return $decodedString;
+}
+
+?>
